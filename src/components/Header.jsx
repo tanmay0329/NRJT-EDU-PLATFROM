3ed4d0658f42d480b79e0f3e5745d034df74a03f
Tanmay <tanmay.bora221@vit.edu>
2025-12-01 01:28:23 +0530

    Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Bell, X, Plus, Edit2, Trash2, Check } from 'lucide-react';
 
-import logo from '../assets/logo.jpeg';
+import logo from '../assets/logo_nrjt.png';
 
 const Header = ({ 
   isStudentMode, 
